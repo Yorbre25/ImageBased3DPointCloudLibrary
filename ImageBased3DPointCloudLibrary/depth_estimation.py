b2d853e19215eb1a74cdad7d5160267087af8860
Yorbre25 <yraulbr25@gmail.com>
2025-02-05 11:43:53 -0600

    Add: Docstring functions

--- ImageBased3DPointCloudLibrary/depth_estimation.py
+++ ImageBased3DPointCloudLibrary/depth_estimation.py
@@ -3,13 +3,14 @@ from PIL import Image
 import os
 import numpy as np
 
-class DepthEstimationPipelineManager:
+
+class _DepthEstimationPipelineManager:
     _instance = None
     _pipeline = None
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(DepthEstimationPipelineManager, cls).__new__(cls)
+            cls._instance = super(_DepthEstimationPipelineManager, cls).__new__(cls)
         return cls._instance
 
     def get_pipeline(self):
@@ -18,21 +19,38 @@ class DepthEstimationPipelineManager:
         return self._pipeline
 
     def load_pipeline(self):
-        return pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
+        return pipeline(
+            task="depth-estimation",
+            model="depth-anything/Depth-Anything-V2-Small-hf",
+            force_download=True,
+            revision="main",
+        )
+
 
 def depth_estimation_from_image(
-        image: np.ndarray, 
-        api: bool = True,
+    image: np.ndarray,
+    api: bool = True,
 ):
+    """
+    This function returns the depth estimation of an image. There is no unit for the depth estimation; it is just a relative value.
+    The bigger the number, the further the object is from the camera.
+
+    Args:
+        - image (np.ndarray): The input image as a NumPy array.
+        - api (bool): Flag to determine whether to use the API for depth estimation. Default is True.
+
+    Returns:
+        - np.ndarray: The depth estimation of the image as a NumPy array. The NumPy array shape will be bigger than the input image.
+    """
+
     image = Image.fromarray(image)
 
-
     if api:
-        depth_estimation_pipeline = DepthEstimationPipelineManager().get_pipeline()
+        depth_estimation_pipeline = _DepthEstimationPipelineManager().get_pipeline()
         result = depth_estimation_pipeline(image)
         depth_map_tensor = result["predicted_depth"]
         depth_estimation = depth_map_tensor[0].numpy()
     else:
         raise Exception("Not implemented yet")
-        
+
     return depth_estimation
---
+++ docs/ImageBased3DPointCloudLibrary.rst
@@ -0,0 +1,37 @@
+ImageBased3DPointCloudLibrary package
+=====================================
+
+Submodules
+----------
+
+ImageBased3DPointCloudLibrary.depth\_estimation module
+------------------------------------------------------
+
+.. automodule:: ImageBased3DPointCloudLibrary.depth_estimation
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+ImageBased3DPointCloudLibrary.point\_cloud\_aligner module
+----------------------------------------------------------
+
+.. automodule:: ImageBased3DPointCloudLibrary.point_cloud_aligner
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+ImageBased3DPointCloudLibrary.point\_cloud\_generator module
+------------------------------------------------------------
+
+.. automodule:: ImageBased3DPointCloudLibrary.point_cloud_generator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: ImageBased3DPointCloudLibrary
+   :members:
+   :undoc-members:
+   :show-inheritance:
